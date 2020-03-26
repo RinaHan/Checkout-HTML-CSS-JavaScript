79c8e91f5e4f4d3ec3240ddb152f5528452f8bba
= <=>
2020-03-26 02:30:52 -0700

    made item boxes and first interaction

--- comps/Items/items.js
+++ comps/Items/items.js
@@ -1,9 +1,37 @@
+var inum = 0;
 var item_state = [
   {
     title:"New Cell Phone",
     num:0
+  },
+  {
+    title:"Toilet Paper",
+    num:0
+  },
+  {
+    title:"Grocer Package",
+    num:0
   }
-]
+];
+
+
+// var answercheck = [
+//   {title: "a",
+//   name:"3"},
+//   {title: "e",
+//   name:"1"}
+// ];
+function itemboxon(){
+  document.querySelector(".item1").innerHTML = item_state[0].title;
+  document.querySelector(".item2").innerHTML = item_state[1].title;
+  document.querySelector(".item3").innerHTML = item_state[2].title;
+  document.querySelector("#circ1").innerHTML = item_state[0].num;
+  document.querySelector("#circ2").innerHTML = item_state[0].num;
+  document.querySelector("#circ3").innerHTML = item_state[0].num;
+}
+
+
+
 
 function ItemUpdate(){
   //update the item UI with the item_state info
